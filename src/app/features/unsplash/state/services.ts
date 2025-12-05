e42c9195ebb6868096160acc9e50b4219862112b
Edouard <edurand.viel@gmail.com>
2025-12-05 12:32:13 +0100

    environments

--- src/app/features/unsplash/state/services.ts
+++ src/app/features/unsplash/state/services.ts
@@ -1,17 +1,16 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-
+import { environment } from '@environments/environment.development';
 
 @Injectable({ providedIn: 'root' })
 export class UnsplashService {
   private http = inject(HttpClient);
-  private apiUrl = 'https://api.unsplash.com';
-  private ACCESS_KEY = 'HfS6VON2Xod6qiCsWaYwt5DgMrKBUxHmaxcLPlSu2ZI';
+  private unsplashUrl = environment.unsplashUrl;
+  private ACCESS_KEY = environment.accessKey;
 
   searchForPhotos(): Observable<any[]> {
-    const result = this.http.get<any[]>(`${this.apiUrl}/search/photos?query=sky`, {
+    const result = this.http.get<any[]>(`${this.unsplashUrl}/search/photos?query=sky`, {
       headers: {
         Authorization: `Client-ID ${this.ACCESS_KEY}`,
       },
@@ -21,22 +20,22 @@ export class UnsplashService {
   }
 
   getOnePhoto(id: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/photo/${id}`);
+    return this.http.get<any>(`${this.unsplashUrl}/photo/${id}`);
   }
 
   getOnePhotoDetail(id: number) {
-    return this.http.get<any>(`${this.apiUrl}/photo/${id}/detail`);
+    return this.http.get<any>(`${this.unsplashUrl}/photo/${id}/detail`);
   }
 
   createOnePhoto(prod: Partial<any>): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/photo`, prod);
+    return this.http.post<any>(`${this.unsplashUrl}/photo`, prod);
   }
 
   updateOnePhoto(id: string, changes: Partial<any>): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/photo/${id}`, changes);
+    return this.http.put<any>(`${this.unsplashUrl}/photo/${id}`, changes);
   }
 
   deleteOnePhoto(id: string): Observable<{ id: string }> {
-    return this.http.delete<{ id: string }>(`${this.apiUrl}/photo/${id}`);
+    return this.http.delete<{ id: string }>(`${this.unsplashUrl}/photo/${id}`);
   }
 }
